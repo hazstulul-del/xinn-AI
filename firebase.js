4c4c89bd2c394e47639bd23a201b7e100965b0aa
hazstulul-del <hazstulul@gmail.com>
2026-04-25 02:52:22 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
@@ -6,6 +5,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyBqPxbAcHXZF0JLGrvQhuzETiRDmDtyn48",
   authDomain: "xinn-ai.firebaseapp.com",
   projectId: "xinn-ai",
+  storageBucket: "xinn-ai.appspot.com",
+  messagingSenderId: "673308828850",
+  appId: "1:673308828850:web:988c0cec98f223924d15ab"
 };
 
 const app = initializeApp(firebaseConfig);
