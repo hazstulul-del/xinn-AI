deafea11b1f823fb8632d292486616ee5c044668
hazstulul-del <hazstulul@gmail.com>
2026-04-25 02:03:57 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBqPxbAcHXZF0JLGrvQhuzETiRDmDtyn48",
+  authDomain: "xinn-ai.firebaseapp.com",
+  projectId: "xinn-ai",
+  storageBucket: "xinn-ai.firebasestorage.app",
+  messagingSenderId: "673308828850",
+  appId: "1:673308828850:web:988c0cec98f223924d15ab",
+  measurementId: "G-0HW3REJJFJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
