be01a9ce642a72e8b99ebf526f9234905497d571
hazstulul-del <hazstulul@gmail.com>
2026-04-25 09:10:27 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,9 @@
 import { auth, provider } from "./firebase.js";
 import {
-  signInWithRedirect,
-  getRedirectResult,
+  signInWithPopup,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-export async function loginGoogle() {
-  try {
-    await signInWithRedirect(auth, provider);
-  } catch (err) {
-    alert("Login gagal: " + err.message);
-  }
-}
 
 export async function loginGoogle() {
   try {
@@ -35,21 +26,22 @@ export async function loginGoogle() {
 export async function logout() {
   await signOut(auth);
   localStorage.removeItem("xinn_user");
-  window.location.href = "login.html";
+  location.replace("./login.html");
 }
 
 export function checkLogin() {
   onAuthStateChanged(auth, (user) => {
     if (!user) {
       localStorage.removeItem("xinn_user");
-      window.location.href = "login.html";
-    } else {
-      localStorage.setItem("xinn_user", JSON.stringify({
-        uid: user.uid,
-        name: user.displayName,
-        email: user.email,
-        photo: user.photoURL
-      }));
+      location.replace("./login.html");
+      return;
     }
+
+    localStorage.setItem("xinn_user", JSON.stringify({
+      uid: user.uid,
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL
+    }));
   });
 }
